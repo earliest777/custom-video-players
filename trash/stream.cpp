7aba2df68f2ccca1d689a778980f661013ff13f6
earliest777 <benjamin@shiny10k.world>
2022-05-19 17:56:31 -0400

    refactored trash

--- trash/stream.cpp
+++ trash/stream.cpp
@@ -1,32 +1,32 @@
+// Copyright 2022 James T Oswald
 
-#include<string>
-
-extern "C"{
+extern "C" {
 #include<stdio.h>
 #include<stdint.h>
 
 #include <pulse/pulseaudio.h>
 }
 
+#include<string>
 
-std::string audioStreamCmd = "ffmpeg -v error -i ../../assets/mutation.mp3 -ar 44100 -ac 2 -f s16le -";
+const std::string audioStreamCmd = "ffmpeg -v error -i ../assets/sound.mp3 -ar 44100 -ac 2 -f s16le -";
 FILE* audioInputStream;
 
-void streamWriteCallback(pa_stream *p, size_t nbytes, void* userdata){
+void streamWriteCallback(pa_stream *p, size_t nbytes, void* userdata) {
     int16_t* data;
     pa_stream_begin_write(p, (void**)&data, &nbytes);
     fread(data, 1, nbytes, audioInputStream);
-    if(feof(audioInputStream)){ //Looping
+    if(feof(audioInputStream)) { //Looping
         pclose(audioInputStream);
         audioInputStream = popen(audioStreamCmd.c_str(), "r");
     }
     pa_stream_write(p, (void*)data, nbytes, NULL, 0, PA_SEEK_RELATIVE);
 }
 
-void contextStateCallback(pa_context *context, void *userdata){
+void contextStateCallback(pa_context *context, void *userdata) {
     pa_context_state_t state = pa_context_get_state(context);
-    switch(state){
-        case PA_CONTEXT_READY:{
+    switch (state) {
+        case PA_CONTEXT_READY: {
             pa_sample_spec ss;
             ss.format = PA_SAMPLE_S16NE;
             ss.channels = 2;
@@ -43,16 +43,16 @@ void contextStateCallback(pa_context *context, void *userdata){
     }
 }
 
-int main(){
+int main() {
     int error;
     pa_mainloop* mainloop = pa_mainloop_new();
     pa_mainloop_api* mainloopapi = pa_mainloop_get_api(mainloop);
     pa_context* ctx = pa_context_new(mainloopapi, "PulseAudio Test");
-    error = pa_context_connect(ctx, NULL, PA_CONTEXT_NOAUTOSPAWN, NULL);
+    error = pa_context_connect(ctx, "tcp:192.168.128.1", PA_CONTEXT_NOAUTOSPAWN, NULL);
     printf("Error: %s\n", pa_strerror(error));
     pa_context_set_state_callback(ctx, &contextStateCallback, NULL);
-    
-    while(true){
+
+    while (true) {
         pa_mainloop_iterate(mainloop, 1, NULL);
     }
     return 0;
