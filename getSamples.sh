047d64b6886103c76fa406502f3c36c7f175eda1
earliest777 <benjamin@shiny10k.world>
2022-05-19 17:55:54 -0400

    sample script

---
+++ getSamples.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir -p assets
+wget -O assets/sample.mp4 http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4 
+wget -O assets/sound.mp3 https://file-examples.com/storage/feb04797b46286b5ea5f061/2017/11/file_example_MP3_700KB.mp3 
